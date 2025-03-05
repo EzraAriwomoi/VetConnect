9e8d5ebe7ac75f3e1d6f8eb5e75bec769a460c05
Ariwomoi <kropezra@gmail.com>
2025-03-05 23:35:09 +0300

    cors

--- Backend/run.py
+++ Backend/run.py
@@ -1,10 +1,21 @@
-from flask import Flask
+from flask import Flask, jsonify, request
+from flask_cors import CORS
+import mysql.connector
 
 app = Flask(__name__)
+CORS(app)
+
+db = mysql.connector.connect(
+    host="localhost",
+    user="root",
+    password="arish",
+    database="vetconnect_db"
+)
+cursor = db.cursor()
 
 @app.route('/')
 def home():
-    return "Hello, Flask!"
+    return jsonify({"message": "VetConnect API is running!"})
 
 if __name__ == '__main__':
     app.run(debug=True)
